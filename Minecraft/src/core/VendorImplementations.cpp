b5e4056aea41361386bf08f35591bbb8a7730f3a
Gabe Ambrosio <gamesbygabe1@gmail.com>
2021-08-12 14:12:30 -0500

    Add logger as submodule

---
+++ Minecraft/src/core/VendorImplementations.cpp
@@ -0,0 +1,15 @@
+#define GABE_LOGGER_IMPL
+#include <logger/logger.h>
+#undef GABE_LOGGER_IMPL
+
+#define GABE_CPP_UTILS_IMPL
+#include <CppUtils/CppUtils.h>
+#undef GABE_CPP_UTILS_IMPL
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+#undef STB_IMAGE_IMPLEMENTATION
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_write.h>
+#undef STB_IMAGE_WRITE_IMPLEMENTATION
